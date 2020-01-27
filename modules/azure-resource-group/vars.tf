ad828a20b461b9cd894869ef5d2af7b3d4c45a44
Luke Fritz <lukefritz@gmail.com>
2020-01-27 15:27:44 -0600

    Initial commit

---
+++ modules/azure-resource-group/vars.tf
@@ -0,0 +1,9 @@
+variable "location" {
+  description = "The Azure region in which this resource group will be deployed."
+  type        = string
+}
+
+variable "name" {
+  description = "The name to assign to the resource group."
+  type        = string
+}
