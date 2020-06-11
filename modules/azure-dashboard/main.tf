a4ff554d709b3fa3ad95e449937a48999f735cef
Luke Fritz <lukefritz@gmail.com>
2020-06-11 15:46:34 -0500

    Remove unused variable; update to conform to style guidelines (feat)

--- modules/azure-dashboard/main.tf
+++ modules/azure-dashboard/main.tf
@@ -1,20 +1,21 @@
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-# DEPLOY AZURE SHARED DASHBOARDS FOR A SUBSCRIPTION
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# ---------------------------------------------------------------------------------------------------------------------
+# DEPLOY AN AZURE SHARED DASHBOARD
+# ---------------------------------------------------------------------------------------------------------------------
 
 terraform {
   required_version = ">= 0.12"
 }
 
-resource "azurerm_dashboard" "dash" {
-  name                = var.name
-  resource_group_name = var.resource_group_name
-  location            = var.location
+resource "azurerm_dashboard" "dashboard" {
+  name                 = var.name
+  resource_group_name  = var.resource_group_name
+  location             = var.location
+  dashboard_properties = var.dashboard_properties
+
   tags = merge(
     {
       "terraform-managed" = "true"
     },
     var.tags
   )
-  dashboard_properties = var.dashboard_properties
 }
--- modules/azure-dashboard/vars.tf
+++ modules/azure-dashboard/vars.tf
@@ -1,31 +1,25 @@
 variable "location" {
-  description = "The Azure region in which azure dashboards will be deployed."
+  description = "The Azure region in which this dashboard will be deployed."
   type        = string
 }
 
 variable "name" {
-  description = "The name to assign to the Azure Dashboards."
+  description = "Specifies the name of the Shared Dashboard"
   type        = string
-  default     = "Quantum Security Platform"
 }
 
 variable "resource_group_name" {
-  description = "The name of the resource group in which azure dashboards will be provisioned."
+  description = "The name of the resource group in which this dashboard will be provisioned."
   type        = string
 }
 
 variable "tags" {
-  description = "(Optional) A mapping of tags to assign to the resource."
+  description = "A mapping of tags to assign to the resource."
   type        = map(string)
   default     = {}
 }
 
-variable "logo" {
-  description = "This is to white label Azure dashboards with Quantum logo."
-  type        = string
-  default     = "https://www.quantum.security/assets/img/quantum-logo.svg"
-}
-
 variable "dashboard_properties" {
-  description = "This is the JSON template for azure dashboard configuration."
+  description = "JSON data representing the dashboard body."
+  type        = string
 }
