eb7eb888e8a25c588e6b5a9fa9bb22c2171623b3
Luke Fritz <lukefritz@gmail.com>
2021-01-10 00:53:18 -0600

    Add Lighthouse delegation module (feat)

---
+++ modules/azure-lighthouse-delegation/main.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_version = ">= 0.12.26"
+}
+
+resource "azurerm_lighthouse_definition" "definition" {
+  name               = var.name
+  description        = var.description
+  managing_tenant_id = var.managing_tenant_id
+
+  # This name may be misleading. Currently operating on an assumption based on its usage within the provider.
+  # See https://github.com/terraform-providers/terraform-provider-azurerm/issues/10120
+  scope = var.subscription_id
+
+  dynamic "authorization" {
+    for_each = var.authorizations
+    content {
+      principal_id       = authorization.value.principal_id
+      role_definition_id = authorization.value.role_definition_id
+    }
+  }
+}
+
+resource "azurerm_lighthouse_assignment" "assignment" {
+  for_each = var.scopes
+
+  scope                    = each.value
+  lighthouse_definition_id = azurerm_lighthouse_definition.definition.id
+}
