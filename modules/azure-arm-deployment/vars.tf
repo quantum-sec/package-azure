18f60bf723bd42ded465735a10020abd9edd0bf0
hensonto <hensonto@gmail.com>
2021-02-01 16:46:54 +0800

    XDR-60: Fix base on code review

--- modules/azure-arm-deployment/vars.tf
+++ modules/azure-arm-deployment/vars.tf
@@ -4,22 +4,22 @@ variable "resource_group_name" {
 }
 
 variable "deployment_mode" {
-  description = "Specifies the mode that is used to deploy resources"
+  description = "Specifies the mode that is used to deploy resources."
   type        = string
 }
 
 variable "name" {
-  description = "The name which should be used for this Resource Group Template Deployment"
+  description = "The name which should be used for this Resource Group Template Deployment."
   type        = string
 }
 
 variable "arm_script" {
-  description = "The contents of the ARM Template which should be deployed into this Resource Group"
+  description = "The contents of the ARM Template which should be deployed into this Resource Group."
   type        = string
 }
 
 variable "parameters_override" {
-  description = "The contents of the ARM Template parameters file - containing a JSON list of parameters"
-  type        = map
+  description = "The contents of the ARM Template parameters file - containing a JSON list of parameters."
+  type        = map(any)
   default     = {}
 }
