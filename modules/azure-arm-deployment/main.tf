7e82ebfb76add33471bd9dca8f9a7f021275d098
Luke Fritz <lukefritz@gmail.com>
2021-02-05 01:32:40 -0600

    Ignore lifecycle changes to parameters (fix)

--- modules/azure-arm-deployment/main.tf
+++ modules/azure-arm-deployment/main.tf
@@ -15,6 +15,13 @@ terraform {
 }
 
 resource "azurerm_resource_group_template_deployment" "resource_group_deploy" {
+  lifecycle {
+    # The use of `SecureString`-typed parameters causes a perma-diff (the Azure API always returns
+    # an empty string). We're ignoring changes to this property, but you can track these changes
+    # by templating a hash of the parameters into the template itself.
+    ignore_changes = [parameters_content]
+  }
+
   name                = var.name
   resource_group_name = var.resource_group_name
   deployment_mode     = var.deployment_mode
