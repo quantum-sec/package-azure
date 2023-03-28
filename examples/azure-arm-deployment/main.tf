58933af63952d2a70e4bed6adac720921352d5ca
nicholascwd <56076256+nicholascwd@users.noreply.github.com>
2023-03-28 08:40:53 +0000

    XDR-4339: Satisfy updated tflint requirements (patch)

--- examples/azure-arm-deployment/main.tf
+++ examples/azure-arm-deployment/main.tf
@@ -1,3 +1,13 @@
+terraform {
+  required_version = ">= 0.12"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.0"
+    }
+  }
+}
 module "deploy_web_app_sample" {
   source = "../../modules/azure-arm-deployment"
 
