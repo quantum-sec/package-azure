b89d9c045bcde7d25a053cb18bfed5855043a6c6
nicholascwd <56076256+nicholascwd@users.noreply.github.com>
2023-03-28 08:20:19 +0000

    XDR-4339: Add support for tags in azurerm resource group (feat)

--- modules/azure-resource-group/main.tf
+++ modules/azure-resource-group/main.tf
@@ -4,9 +4,17 @@
 
 terraform {
   required_version = ">= 0.12"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.0"
+    }
+  }
 }
 
 resource "azurerm_resource_group" "resource_group" {
   name     = var.name
   location = var.location
+  tags     = var.tags
 }
